e769e933bd51abb8ad9a877c65a19ac885786303
shahid13384 <shahid13384@hotmail.com>
2023-07-10 22:54:38 +0530

    Entity Object

---
+++ 03-spring-boot-jpa/01-curddemo-student/src/main/java/com/zaynsys/cruddemo/entity/Student.java
@@ -0,0 +1,84 @@
+package com.zaynsys.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    // define fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String fistName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    // define constructor
+
+
+    public Student() {
+    }
+
+    public Student(String fistName, String lastName, String email) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    // define greater/setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    //Generate ToString method
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", fistName='" + fistName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
