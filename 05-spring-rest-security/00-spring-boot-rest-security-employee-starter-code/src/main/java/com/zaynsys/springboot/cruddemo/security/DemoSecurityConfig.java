74ec5d64bec77753ac5d0d04415fc39af95d22ff
shahid13384 <shahid13384@hotmail.com>
2023-07-29 19:06:17 +0530

    database user authentication

--- 05-spring-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/zaynsys/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/zaynsys/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,36 +5,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+  // add support for JDBC.... no more hardcoded users
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+  @Bean
+  public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+      return new JdbcUserDetailsManager(dataSource);
+  }
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+
+
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -49,7 +65,7 @@ public class DemoSecurityConfig {
         http.httpBasic(Customizer.withDefaults());
 
         // disable Cross site request Forgery (CSRF)
-        http.csrf(csrf->csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
 
