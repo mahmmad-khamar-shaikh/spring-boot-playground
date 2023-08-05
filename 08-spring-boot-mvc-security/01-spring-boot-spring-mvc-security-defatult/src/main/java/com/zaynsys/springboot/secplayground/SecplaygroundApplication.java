ceef41a758bf9e8f4061597372bf3a0e8107b152
shahid13384 <shahid13384@hotmail.com>
2023-08-05 19:04:40 +0530

    InMemory database authentication

--- 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-defatult/src/main/java/com/zaynsys/springboot/secplayground/SecplaygroundApplication.java
+++ 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-defatult/src/main/java/com/zaynsys/springboot/secplayground/SecplaygroundApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecplaygroundApplication {
+public class  SecplaygroundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecplaygroundApplication.class, args);
