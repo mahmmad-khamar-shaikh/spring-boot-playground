a5109abe75946a2d10dbfe87520a6c885774db7a
shahid13384 <shahid13384@hotmail.com>
2023-08-07 08:14:50 +0530

    Filterchain added

--- 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-defatult/src/main/java/com/zaynsys/springboot/secplayground/security/DemoSecurityConfig.java
+++ 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-defatult/src/main/java/com/zaynsys/springboot/secplayground/security/DemoSecurityConfig.java
@@ -2,14 +2,16 @@ package com.zaynsys.springboot.secplayground.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
                 .username("john")
                 .password("{noop}test123")
@@ -20,15 +22,27 @@ public class DemoSecurityConfig {
         UserDetails mary = User.builder()
                 .username("may")
                 .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
+                .roles("EMPLOYEE", "MANAGER")
                 .build();
 
         UserDetails susan = User.builder()
                 .username("susan")
                 .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(john,mary,susan);
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(config -> config.anyRequest().authenticated())
+                .formLogin(form ->
+                        form.loginPage("/showLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+        return http.build();
+
     }
 }
